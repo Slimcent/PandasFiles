87fc244f23cfd1fd071c6ae0b9feaf60971fa245
Slimcent <centpresley@gmail.com>
2023-08-07 21:48:11 +0100

    Viwing csv and json files using pandas

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+df = pd.read_csv('data.csv')
+print(df)
 
+print(pd.options.display.max_rows)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pd.options.display.max_rows = 9999
+df = pd.read_csv('data.csv')
+print(df)
 
+print()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+df = pd.read_csv('data.csv')
+print(df)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print()
+
+df = pd.read_json('data.json')
+print(df.to_string())
+
+print()
+
+data = {
+    "Duration": {
+        "0": 60,
+        "1": 60,
+        "2": 60,
+        "3": 45,
+        "4": 45,
+        "5": 60
+    },
+    "Pulse": {
+        "0": 110,
+        "1": 117,
+        "2": 103,
+        "3": 109,
+        "4": 117,
+        "5": 102
+    },
+    "Maxpulse": {
+        "0": 130,
+        "1": 145,
+        "2": 135,
+        "3": 175,
+        "4": 148,
+        "5": 127
+    },
+    "Calories": {
+        "0": 409,
+        "1": 479,
+        "2": 340,
+        "3": 282,
+        "4": 406,
+        "5": 300
+    }
+}
+df = pd.DataFrame(data)
+print(df)
